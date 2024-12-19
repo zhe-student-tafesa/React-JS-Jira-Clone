73cba44a492119e717a9d32d1b1ac54ba8508a7c
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-12-19 16:16:52 +1000

    1. install next-themes 2. set dark mode theme

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,7 @@
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 export const metadata = {
   title: "Jira",
@@ -21,9 +12,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
+
       </body>
     </html>
   );
